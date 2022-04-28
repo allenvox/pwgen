f5106a039b84b99582ab5af7fc172148ae113c39
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-04-28 22:46:29 +0700

    Separate generatePassword() to external module

--- src/main.c
+++ src/main.c
@@ -1,69 +1,15 @@
 #include "alphabetic.h"
 #include "input.h"
 #include "random.h"
+#include "password.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-char* generatePassword(struct Option option)
-{
-    int size = option.size;
-    char* password = malloc(sizeof(char) * size);
-    int* cells = malloc(sizeof(int) * size);
-
-    for (int i = 0; i < size; i++) {
-        cells[i] = 0;
-    }
-
-    if (option.numeric == 0 && option.capitalized == 0 && option.special == 0) {
-        for (int i = 0; i < size; i++) {
-            password[i] = getLowercase(i);
-        }
-
-    } 
-    if (option.capitalized == 1) {
-        int guaranteed = getRandom(0, size - 1, 0);
-        if (password[guaranteed] == 1) {
-            while (password[guaranteed] != 0) {
-                int i = 1;
-                guaranteed = getRandom(0, size - 1, i);
-                i++;
-            }
-        }
-        password[guaranteed] = getCapital(0);
-        cells[guaranteed] = 1;
-
-        for (int i = 0; i < size; i++) {
-            if (cells[i] == 0) {
-                password[i] = getFromAlphabet(0, 1, 0, i);
-            }
-        }
-    }
-    if (option.numeric == 1){
-        int guaranteed = getRandom(0, size - 1, 0);
-        if (password[guaranteed] == 1) {
-            while (password[guaranteed] != 0) {
-                int i = 1;
-                guaranteed = getRandom(0, size - 1, i);
-                i++;
-            }
-        }
-        password[guaranteed] = getNumber(0);
-        cells[guaranteed] = 1;
-
-        for (int i = 0; i < size; i++) {
-            if (cells[i] == 0) {
-                
-                password[i] = getFromAlphabet(option.numeric, option.capitalized, option.special, i);
-            }
-        }
-    }
-    free(cells);
-    return password;
-}
+#include <time.h>
 
 int main(int argc, char** argv)
 {
+    srand(time(NULL));
     struct Option option = initOptions();
     option = getOptions(option, argc, argv);
     char* result;
---
+++ src/password.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "input.h"
+
+int getGuaranteedIndex(int* cells, int size);
+char* generatePassword(struct Option option);
