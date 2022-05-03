cf2faf8e338bd64a8de52c6255ff1ff59f1364bd
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-05-03 11:55:21 +0700

    Format code

--- test/test.c
+++ test/test.c
@@ -1,8 +1,8 @@
-#include <ctest.h>
 #include "random.h"
+#include <ctest.h>
 
 CTEST(check_random, valid)
-{   
+{
     int value = getRandom(0, 100);
     int result = value > 0 && value < 100;
     int expected = 1;
