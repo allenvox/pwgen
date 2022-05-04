85d3f197ebdbf568101c7f8cb771f6ce96cf4f2e
alexichab <naumchik199@mail.ru>
2022-05-04 15:56:16 +0700

    Make getRandom test

--- test/test.c
+++ test/test.c
@@ -1,5 +1,6 @@
 #include "random.h"
 #include <ctest.h>
+#include "input.h"
 
 CTEST(check_random, valid)
 {
@@ -8,3 +9,19 @@ CTEST(check_random, valid)
     int expected = 1;
     ASSERT_EQUAL(expected, result);
 }
+
+CTEST(check_random, invalid)
+{
+    int value = getRandom(0, 100);
+    int result = value < 0 || value > 100;
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(check_isnumber, valid)
+{
+    int result = isNumber("1000");
+    int expected = 1;
+    ASSERT_EQUAL(expected, result);
+}
+
