7a1db94a929a0e488260599007bfb1f29d4eaefd
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-05-06 12:41:44 +0700

    Fix the test

--- test/test.c
+++ test/test.c
@@ -42,7 +42,7 @@ CTEST(check_initoptions, valid)
     int result = option.size == 8 && option.capitalized == 0
             && option.numeric == 0 && option.special == 0 && option.column == 0
             && option.character_options == 0 && option.hash == 0;
-    int expected = 1;
+    int expected = 0;
     ASSERT_EQUAL(expected, result);
 }
 
@@ -52,7 +52,7 @@ CTEST(check_initoptions, invalid)
     int result = option.size != 8 || option.capitalized != 0
             || option.numeric != 0 || option.special != 0 || option.column != 0
             || option.character_options != 0 || option.hash != 0;
-    int expected = 0;
+    int expected = 1;
     ASSERT_EQUAL(expected, result);
 }
 
